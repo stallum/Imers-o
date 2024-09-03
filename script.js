5d3351d14acad4a25aacd1d6308d56e28bd9afb2
artur@stallum <arturpess844@gmail.com>
2024-09-03 14:48:50 -0300

    alterações no JS, CSS e HTML

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Olá, mundo");
+const  searchInput = document.querySelector('input[type="text"]');
+const searchButton = document.querySelector('button[type="submit"]');
+const resultsContainer = document.querySelector('.item-resultado');
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value;
+})
